d012b171e483c4ce57415ad2fde177765d6b4892
zanid <zanid227@yandex.ru>
2025-01-09 15:12:49 +0300

    add config dataclass for DB

---
+++ core/config/db_config.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class DBConfig:
+    db_host: str
+    db_port: str
+    db_password: str
+    db_user: str
+    db_name: str
+
+
+def load_config(path: str | None = None) -> DBConfig:
+    env = Env()
+    env.read_env(path)
+    return DBConfig(
+        db_host=env.str("DATABASE_HOST"),
+        db_port=env.str("DATABASE_PORT"),
+        db_password=env.str("DATABASE_PWD"),
+        db_user=env.str("PG_USER"),
+        db_name=env.str("PG_NAME"),
+    )
