69ad5c2606ca675da778e5a33d6fd8646207b685
zanid <zanid227@yandex.ru>
2025-01-09 19:21:49 +0300

    add docs in config file

--- core/config/db_config.py
+++ core/config/db_config.py
@@ -4,6 +4,10 @@ from environs import Env
 
 @dataclass
 class DBConfig:
+    """
+    Класс DBConfig, декорируемый дата-классом,
+    для сигнатуры хранения переменных окружения базы данных
+    """
     db_host: str
     db_port: str
     db_password: str
