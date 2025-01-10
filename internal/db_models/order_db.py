594553e8c80a13fb537cdc3ea6ab81f87c42a29d
zanid <zanid227@yandex.ru>
2025-01-10 16:54:03 +0300

    create all models with declarative_base for migrate to db

---
+++ internal/db_models/order_db.py
@@ -0,0 +1,22 @@
+from internal.db_models.core import Base
+
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class Order(Base):
+
+    __tablename__ = 'orders'
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey('products.id', ondelete='SET NULL'), nullable=False
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id', ondelete='CASCADE'), nullable=False
+    )
+
+    products = relationship('Product', back_populates='orders')
+    user = relationship('User', back_populates='orders')
---
+++ internal/db_models/tag_db.py
@@ -0,0 +1,22 @@
+from internal.db_models.core import Base
+from internal.db_models.product_db import Product
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+
+
+class Tag(Base):
+
+    __tablename__ = 'tags'
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    title: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False
+    )
+    description: Mapped[str] = mapped_column(
+        String(255), nullable=True
+    )
+
+    products = relationship(Product, back_populates='tag')
