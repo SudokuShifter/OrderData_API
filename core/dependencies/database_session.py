04420b0768c6637c124830609f4dd879b2920783
zanid <zanid227@yanbex.ru>
2025-01-22 19:27:18 +0300

    add docs in func get_db

--- core/dependencies/database_session.py
+++ core/dependencies/database_session.py
@@ -5,6 +5,10 @@ from core.dependencies.database import session_manager
 
 
 async def get_db() -> AsyncIterator[AsyncSession]:
+    """
+    Функция get_db возвращает асинхронный генератор.
+    Этот генератор можно использовать для получения объектов типа AsyncSession.
+    """
     session = session_manager.session()
     if session is None:
         raise Exception('DatabaseSessionManager has not been initialized')
