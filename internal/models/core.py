8178f0b46121bd2b3aed7ef85f4365ca597ae2a2
zanid <zanid227@yanbex.ru>
2025-01-22 19:30:20 +0300

    add docs in in model pydantic-core

--- internal/models/core.py
+++ internal/models/core.py
@@ -3,10 +3,16 @@ from pydantic import BaseModel
 
 
 class CoreModel(BaseModel):
+    """
+    Core-модель для pydantic-схем без id
+    """
     pass
 
 
 class IDMixCoreModel(BaseModel):
+    """
+    Core-модель для pydantic-схем с id
+    """
     id: ClassVar[int]
 
 
