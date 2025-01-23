d4c8a2189c46183ad54cf43e9cb93ba8d8820e80
zanid <zanid227@yanbex.ru>
2025-01-23 18:43:18 +0300

    continue work to Products

--- internal/repository/orders.py
+++ internal/repository/orders.py
@@ -1,3 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import or_
+from passlib.hash import pbkdf2_sha256
+from typing import List
+
 
 class OrderRepository:
-    pass
+
+     @staticmethod
+     async def get_all():
+         pass
